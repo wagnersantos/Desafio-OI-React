3de4078f0209dbcb9bbf2c89d22d771030d90c04
wagnersantos <wagtvc@gmail.com>
2019-01-09 14:01:06 -0200

    adicionando componente Header

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../../components/header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        Teste
+        <Header />
       </div>
     );
   }
---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+    	<header>
+	    	<div className="container-menu">   
+	    	Teste     
+	      	</div>
+     	</header>
+    );
+  }
+}
+
+export default Header;
