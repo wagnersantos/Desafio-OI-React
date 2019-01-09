1fd5f843ca16740a5d14fa1e9e70c4660898c306
wagnersantos <wagtvc@gmail.com>
2019-01-09 16:05:32 -0200

    adcionando componente Cards

---
+++ src/components/cards/Cards.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Cards extends Component {
+  render() {
+    return (
+     	<div className="container-main--cards">   
+	    </div>
+    );
+  }
+}
+
+export default Cards;
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Title from '../../components/title/Title';
 import Subtitle from '../../components/subtitle/Subtitle';
+import Cards from '../../components/cards/Cards';
 
 class Main extends Component {
   render() {
     return (
     	<div className="container-main"> 
     		<Title /> 
-    		<Subtitle /> 
+    		<Subtitle />
+    		<Cards /> 
       	</div>
     );
   }
