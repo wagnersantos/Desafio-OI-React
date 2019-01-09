13a702596cdfc93e75cf1ad8fdda2564e12c49a5
wagnersantos <wagtvc@gmail.com>
2019-01-09 15:34:33 -0200

    adcionando componente Subtitle

---
+++ src/components/subtitle/Subtitle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Subtitle = (props) => (
+	<div className="container-main--subtitle">
+		<h2>Todas as nossas ofertas:</h2>
+	</div>
+
+)
+
+export default Subtitle;
