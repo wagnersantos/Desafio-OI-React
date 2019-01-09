57ec08771cf035a579e1d869097a1bb2695358d4
wagnersantos <wagtvc@gmail.com>
2019-01-09 14:55:09 -0200

    adcionando componente main

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../../components/header/Header';
+import Main from '../../components/main/Main';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Header />
+        <Main />
       </div>
     );
   }
---
+++ src/components/main/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+    	<header>
+	    	<div className="container-main">   
+	      	</div>
+     	</header>
+    );
+  }
+}
+
+export default Main;
