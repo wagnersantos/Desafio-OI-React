80d71f96463c222de43773b603e0f4f5e8cee235
wagnersantos <wagtvc@gmail.com>
2019-01-10 15:19:45 -0200

    adicionando método handleChange ao componente Select

--- src/components/select/Select.js
+++ src/components/select/Select.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardsData from '../../services/cardsData/CardsData';
 
-const Select = (props) => (
-	<div className="container-menu--select">
-		<select>
-		  	<option value="Todos">Todos</option>
-		  	<option value="Rio de Janeiro">Rio de Janeiro</option>
-		  	<option value="São Paulo">São Paulo</option>
-		</select>
-	</div>
-
-)
+class Select extends Component {
+	constructor(props){
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(value){
+	}
+	render(){
+		return (
+			<div className="container-menu--select" onChange={(e) => this.handleChange(e.target.value)}>
+				<select>
+				  	<option value="Todos">Todos</option>
+				  	<option value="Rio de Janeiro">Rio de Janeiro</option>
+				  	<option value="São Paulo">São Paulo</option>
+				</select>
+			</div>
+		)
+	}
+}
 
 export default Select;
