39c0024491bed6f7984263ec84bac7c5d4697aa9
wagnersantos <wagtvc@gmail.com>
2019-01-15 10:21:23 -0200

    corrigindo nome de dependentes e retirando bold da fonte

--- src/components/cardsParts/CardsDependents.js
+++ src/components/cardsParts/CardsDependents.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CardsDependents = ({arr}) => (
     ((arr['dependente-preco'].length !== 0) &&
     (<div className='cards__dependents'>
-        <p className='cards__dependents-title'>Adicione depentendes:</p>
+        <p className='cards__dependents-title'>Adicione dependentes:</p>
         <p className='cards__dependents-price'>{`Por mais R$ ${arr['dependente-preco']} cada`}</p>
     </div>))
     || 
